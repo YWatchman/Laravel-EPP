e692b3162c4db6197a85c160dc8328a73125b273
Yvan E. Watchman <ywatchman@cyberfusion.nl>
2019-08-18 09:01:28 +0200

    publish config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,6 +8,16 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      * Bootstrap application events
      */
     public function boot() {
-        //
+        $this->publishConfig();
+    }
+
+    private function publishConfig()
+    {
+        $path = $this->getConfigPath();
+        $this->publishes([$path => config_path('laravel-epp.php')], 'config');
+    }
+    private function getConfigPath()
+    {
+        return __DIR__ . '/../../config/laravel-epp.php';
     }
 }
