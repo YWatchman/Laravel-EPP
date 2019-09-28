0073bd413dc51f1ddd47cfe70b453488cf59a890
Yvan E. Watchman <ywatchman@cyberfusion.nl>
2019-09-28 20:18:58 +0200

    check contact

---
+++ src/Epp/Contact.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace YWatchman\LaravelEPP\Epp;
+
+use Metaregistrar\EPP\eppCheckContactRequest;
+use Metaregistrar\EPP\eppCheckContactResponse;
+use Metaregistrar\EPP\eppConnection;
+use Metaregistrar\EPP\eppContactHandle;
+use Metaregistrar\EPP\eppException;
+
+class Contact extends Connection
+{
+
+    /** @var bool|eppConnection|mixed $epp Constructed eppConnection */
+    private $epp;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->epp = $this->getConnection();
+    }
+
+    /**
+     * @param $handle
+     * @return bool
+     * @throws \Metaregistrar\EPP\eppException
+     */
+    public function checkContact($handle)
+    {
+//        $eppHandle = new eppCheckContactRequest(new eppContactHandle($handle));
+        $eppHandle = new eppCheckContactRequest(new eppContactHandle($handle));
+        /** @var eppCheckContactResponse $res */
+        try {
+            $this->epp->request($eppHandle);
+            return true;
+        } catch (eppException $e) {
+            return false;
+        }
+    }
+
+}
