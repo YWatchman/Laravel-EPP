66b8742925cf89a49f1a131beda5af5bb1c02318
Yvan E. Watchman <ywatchman@cyberfusion.nl>
2019-09-28 20:52:32 +0200

    Create contact

--- src/Epp/Contact.php
+++ src/Epp/Contact.php
@@ -5,8 +5,13 @@ namespace YWatchman\LaravelEPP\Epp;
 use Metaregistrar\EPP\eppCheckContactRequest;
 use Metaregistrar\EPP\eppCheckContactResponse;
 use Metaregistrar\EPP\eppConnection;
+use Metaregistrar\EPP\eppContact;
 use Metaregistrar\EPP\eppContactHandle;
+use Metaregistrar\EPP\eppContactPostalInfo;
+use Metaregistrar\EPP\eppCreateContactRequest;
+use Metaregistrar\EPP\eppCreateContactResponse;
 use Metaregistrar\EPP\eppException;
+use Metaregistrar\EPP\sidnEppCreateContactRequest;
 
 class Contact extends Connection
 {
@@ -32,6 +37,38 @@ class Contact extends Connection
         try {
             $this->epp->request($eppHandle);
             return true;
+        } catch (eppException $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Create contact
+     *
+     * @param $name
+     * @param $email
+     * @param $phone
+     * @param $city
+     * @param $countryCode
+     * @param $street
+     * @param $province
+     * @param $zip
+     * @param $org
+     * @param string $registrar
+     * @return bool|eppContactHandle
+     */
+    public function createContact($name, $email, $phone, $city, $countryCode, $street, $province, $zip, $org = null, $registrar = 'sidn')
+    {
+        $eppPostalInfo = new eppContactPostalInfo($name, $city, $countryCode, $org, $street, $province, $zip, 'loc');
+        try {
+            if($registrar == 'sidn') {
+                $eppContact = new sidnEppCreateContactRequest(new eppContact($eppPostalInfo, $email, $phone));
+            } else {
+                $eppContact = new eppCreateContactRequest(new eppContact($eppPostalInfo, $email, $phone));
+            }
+            /** @var eppCreateContactResponse $contact */
+            $contact = $this->epp->request($eppContact);
+            return $contact->getContactHandle();
         } catch (eppException $e) {
             return false;
         }
