a001b3c13451a0f674f8bcabb28c5aa1944d4a7f
William Edwards <wedwards@cyberfusion.nl>
2019-11-09 21:23:16 +0100

    Remove public variables

--- src/YWatchman/LaravelEPP/Models/Contact.php
+++ src/YWatchman/LaravelEPP/Models/Contact.php
@@ -7,35 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     /** @var $id integer Registrant id */
-    public $id;
 
-    /** @var $contactId integer Epp service contact id */
-    public $contactId;
+    /** @var $contact_id integer Epp service contact id */
 
     /** @var $handle string Epp service contact handle */
-    public $handle;
 
     /** @var $email string Contact Email Address */
-    public $email;
 
     /** @var $phone string ITU E.164 Phone number */
-    public $phone;
 
     /** @var $name string Person's full name */
-    public $name;
 
     /** @var $organization string|null Organization name, not required */
-    public $organization;
 
     /** @var $address string Street address including number */
-    public $address;
 
     /** @var $postal string Postal code */
-    public $postal;
 
     /** @var string City */
-    public $city;
 
     /** @var string Country ISO 3166-1 2 char */
-    public $country;
 }
