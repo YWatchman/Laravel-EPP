7a0f82f85bb788c79ba917cb01e288ff6daac233
William Edwards <wedwards@cyberfusion.nl>
2019-11-09 21:23:39 +0100

    Remove public variables

--- src/YWatchman/LaravelEPP/Models/Domain.php
+++ src/YWatchman/LaravelEPP/Models/Domain.php
@@ -7,20 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Domain extends Model
 {
     /** @var $name string Domain name */
-    public $name;
 
     /** @var $registrant integer Registrant contact id */
-    public $registrant;
 
     /** @var $admin integer Admin contact id */
-    public $admin;
 
     /** @var $tech integer Tech contact id */
-    public $tech;
 
     /** @var $billing integer Billing contact id */
-    public $billing;
 
     /** @var $nameservers array Nameservers in array */
-    public $nameservers;
 }
