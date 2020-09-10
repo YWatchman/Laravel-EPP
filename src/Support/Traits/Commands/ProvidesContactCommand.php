361afe9b129bc88c6df3fdafe4d7e4315fe4a145
Yvan E. Watchman <ywatchman@cyberfusion.nl>
2020-09-10 17:17:30 +0200

    fix recursion cmd

--- src/Support/Traits/Commands/ProvidesContactCommand.php
+++ src/Support/Traits/Commands/ProvidesContactCommand.php
@@ -22,24 +22,78 @@ trait ProvidesContactCommand
             new DOMAttr('xmlns:contact', self::NAMESPACE)
         );
 
+        // We don't need empty keys.
         ArrayHelper::filterEmpty($contact);
 
         foreach ($contact as $tag => $child) {
             if (is_array($child)) {
-                $this->walknode = $this->createElement($this->getCommandTag($tag));
-                if (isset($child['attributes'])) {
-                    $this->setAttributes($child['attributes']);
-                    unset($child['attributes']);
-                }
-                $this->recurseTags($child, $this->walknode);
-                $node->appendChild($this->walknode);
-                continue;
+                $tempnode = $this->createElement($this->getCommandTag($tag));
+                $this->recurse($child, $tempnode);
+            } else {
+                $tempnode = $this->createElement(
+                    $this->getCommandTag($tag),
+                    $child
+                );
             }
 
-            // Just append string only tags
-            $node->appendChild($this->createElement($this->getCommandTag($tag), $child));
+            $node->appendChild($tempnode);
         }
 
         return $node;
+    }
+
+    /**
+     * Recurse tags for child.
+     *
+     * @param array $childTags
+     * @param DOMElement $node
+     */
+    private function recurse(array &$childTags, DOMElement $node): void
+    {
+        if (isset($childTags['attributes'])) {
+            // Loop over attributes and add them to the element.
+            foreach ($childTags['attributes'] as $attribute => $value) {
+                $node->setAttribute($attribute, $value);
+            }
+            unset($childTags['attributes']);
+        }
+
+        // This function is created to do recursion...
+        foreach ($childTags as $tag => $child) {
+            if (is_array($child)) {
+                $tempnode = $this->createElement(
+                    $this->getCommandTag($tag)
+                );
+                foreach ($child as $key => $value) {
+                    if (is_array($value)) {
+                        foreach ($value as $v) {
+                            if (empty($v)) continue;
+                            $tempnode->appendChild(
+                                $this->createElement(
+                                    $this->getCommandTag($key),
+                                    $v
+                                )
+                            );
+                        }
+                    } else {
+                        $tempnode->appendChild(
+                            $this->createElement(
+                                $this->getCommandTag($key),
+                                $value
+                            )
+                        );
+                    }
+                }
+
+                $node->appendChild($tempnode);
+            } else {
+                $node->appendChild(
+                    $this->createElement(
+                        $this->getCommandTag($tag),
+                        $child
+                    )
+                );
+            }
+        }
     }
 }
